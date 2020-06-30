cd88aa3a11e4c5fec541120745474d48dbfd9f29
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-06-30 17:01:59 +0200

    Update remote.cmd

--- remote.cmd
+++ remote.cmd
@@ -1,8 +1,12 @@
 :: auto connection : planned tasks as adminstrator. make sure ssh tunnel works
 ::
+
+
 :: option 1 : connection via pw-protected (standard) user
 ::            add standard-user to remote users
 ::
 :: option 2 : allow connections w/o pw
 ::
-ssh -R 55443:localhost:3389 username@server.com
+
+:: Test ssh tunnel
+ssh -R 55443:localhost:3389 username@server.com printf "\nssh connection works!" || (echo && echo ssh connection failed && pause && exit)
