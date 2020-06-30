81d1e9352269f6c071354070fb80776696437ac4
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-06-30 16:17:01 +0200

    Update remote.cmd

--- remote.cmd
+++ remote.cmd
@@ -1,1 +1,6 @@
+:: auto connection : planned tasks as adminstrator. make sure ssh tunnel works
+::
+:: option 1 : connection via pw-protected (standard) user
+:: option 2 : allow connections w/o pw
+::
 ssh -R 55443:localhost:3389 username@server.com
